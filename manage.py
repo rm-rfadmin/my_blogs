c4f297adf348adde1d63b7d2ab5f56e2bfd0f282
rm-rfadmin <1725869293@qq.com>
2020-08-12 20:49:17 +0800

    测试

---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+
+auth_bp = Blueprint('auth', __name__)
+app.register_blueprint(auth_bp, url_prfix='/auth')
+
+
+app.route('/')
+def test():
+    return 'hello_world'
+
+if __name__ == "__main__":
+    app.run()
